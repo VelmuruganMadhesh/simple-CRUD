1ffd1578cfaf8366fbd50381001fcc5391fea485
Velmurugan.Mathesh <Velmurugan.Mathesh@ACHIT150-PC>
2025-09-04 19:04:36 +0530

    Add project files.

---
+++ simple CRUD/Data/StudentContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using simple_CRUD.Models;
+
+namespace simple_CRUD.Data
+{
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public StudentContext(DbContextOptions<StudentContext> options) : base(options) { }
+    }
+}
---
+++ simple CRUD/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace simple_CRUD.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; } = string.Empty;
+        public int age { get; set; }
+        public string email { get; set; }= string.Empty;
+        public string phone { get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
+        public bool isactive { get; set; }
+    }
+}
