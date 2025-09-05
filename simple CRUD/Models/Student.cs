bfa4518b54f4713c393b8ea5c0695d21ca8a4766
Velmurugan.Mathesh <Velukavitha005@gmail.com>
2025-09-05 12:10:41 +0530

    newstatusfeild

--- simple CRUD/Models/Student.cs
+++ simple CRUD/Models/Student.cs
@@ -9,5 +9,6 @@
         public string phone { get; set; } = string.Empty;
         public string password { get; set; } = string.Empty;
         public bool isactive { get; set; }
+        public DateTime DOB { get; set; }
     }
 }
